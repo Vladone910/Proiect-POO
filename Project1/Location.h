7e69b17f8411660bf184401a45da9869e76823f6
leonte <leontevlad21@stud.ase.ro>
2023-11-23 21:54:06 +0200

    Updated the Location class : overloaded operators , copy constructor and default constructor

--- Project1/Location.h
+++ Project1/Location.h
@@ -13,6 +13,12 @@ private:
 
 public:
    
+    Location() : Max_No_Rows(100), Max_No_Seats(200){
+    
+       seatsPerRow = new int[Max_No_Rows];
+    }
+
+
     Location(unsigned int rows, unsigned int maxSeatsPerRow) : Max_No_Rows(rows), Max_No_Seats(maxSeatsPerRow){
     
         seatsPerRow = new int[Max_No_Rows];
@@ -26,6 +32,15 @@ public:
         delete[] seatsPerRow;
     }
 
+
+    Location(const Location& other) : Max_No_Rows(other.Max_No_Rows), Max_No_Seats(other.Max_No_Seats)
+    {
+        seatsPerRow = new int[Max_No_Rows];
+        for (unsigned int i = 0; i < Max_No_Rows; ++i)
+        {
+            seatsPerRow[i] = other.seatsPerRow[i];
+        }
+    }
 
     void displayInfo() const {
     
@@ -78,4 +93,49 @@ public:
             throw exception ("Invalid row number or seats value.\n");
         }
     }
+
+    Location& operator=(const Location& loc){
+    
+        if (this != &loc) {
+
+            if (seatsPerRow != nullptr) {
+
+                delete[] seatsPerRow;
+            }
+
+
+            if (loc.seatsPerRow != nullptr) {
+
+                this->seatsPerRow = new int[Max_No_Rows];
+
+                for (unsigned int i = 0; i < Max_No_Rows; ++i) {
+
+                    this->seatsPerRow[i] = loc.seatsPerRow[i];
+                }
+            }
+         
+            else {
+
+                this->seatsPerRow = nullptr;
+            }
+        }
+            
+        return *this;
+    }
+
+    friend ostream& operator<<(ostream& os, const Location& loc);
 };
+
+
+
+  ostream& operator<<(ostream& os, const Location& loc) {
+    os << "Number of Rows: " << loc.Max_No_Rows << "\n";
+    os << "Maximum Seats per Row: " << loc.Max_No_Seats << "\n";
+    os << "Seats per Row:" << loc.seatsPerRow << "\n";
+
+    for (unsigned int i = 0; i < loc.Max_No_Rows; ++i)
+    {
+        os << " " << loc.seatsPerRow[i];
+    }
+    return os;
+}
